d75858c6cb4e6215090f4667982679d6610ff3d9
ckazu <ckazuyuki@gmail.com>
2014-11-11 01:50:16 +0900

    User Authentication, sign in Google

--- source/app.js.coffee
+++ source/app.js.coffee
@@ -1,1 +1,10 @@
-console.log 'Hello'
+client_id = ''
+project_id = ''
+
+$ ->
+  $('#auth').on 'click', ->
+    gapi.auth.authorize
+      client_id: client_id,
+      scope: 'https://www.googleapis.com/auth/bigquery.readonly',
+      ->
+        gapi.client.load('bigquery', 'v2')
--- source/public/app.js
+++ source/public/app.js
@@ -1,4 +1,19 @@
 (function() {
-  console.log('Hello');
+  var client_id, project_id;
+
+  client_id = '';
+
+  project_id = '';
+
+  $(function() {
+    return $('#auth').on('click', function() {
+      return gapi.auth.authorize({
+        client_id: client_id,
+        scope: 'https://www.googleapis.com/auth/bigquery.readonly'
+      }, function() {
+        return gapi.client.load('bigquery', 'v2');
+      });
+    });
+  });
 
 }).call(this);

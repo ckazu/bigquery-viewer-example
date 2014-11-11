059627a3ac3a91cd4adf28ef359886589a5a8784
ckazu <ckazuyuki@gmail.com>
2014-11-12 01:55:15 +0900

    Request to BigQuery and render data as table

--- source/app.js.coffee
+++ source/app.js.coffee
@@ -2,9 +2,39 @@ client_id = ''
 project_id = ''
 
 $ ->
-  $('#auth').on 'click', ->
+  $('#container').hide()
+
+  $('#auth button').on 'click', ->
     gapi.auth.authorize
-      client_id: client_id,
-      scope: 'https://www.googleapis.com/auth/bigquery.readonly',
+      client_id: client_id
+      scope: 'https://www.googleapis.com/auth/bigquery.readonly'
       ->
         gapi.client.load('bigquery', 'v2')
+        $('#container').show()
+        $('#auth').hide()
+
+  $('#run').on 'click', ->
+    $('#information').text 'loading...'
+
+    if $('#result tbody').size() > 0 # FIXME: replace condition
+      $('#result').dataTable().api().destroy()
+      $('#result').empty()
+
+    request = gapi.client.bigquery.jobs.query
+      projectId: project_id
+      query: $('#query').val()
+
+    request.execute (response) ->
+      if response.error
+        $('#information').text response.message
+      else
+        $('#information').empty()
+        columns = response.schema.fields.map (field) ->
+          { title: field.name }
+        data = _.map response.rows, (row) ->
+          _.map row.f, (values) ->
+            values.v
+
+        $('#result').dataTable
+          data: data
+          columns: columns
--- source/public/app.js
+++ source/public/app.js
@@ -6,12 +6,49 @@
   project_id = '';
 
   $(function() {
-    return $('#auth').on('click', function() {
+    $('#container').hide();
+    $('#auth button').on('click', function() {
       return gapi.auth.authorize({
         client_id: client_id,
         scope: 'https://www.googleapis.com/auth/bigquery.readonly'
       }, function() {
-        return gapi.client.load('bigquery', 'v2');
+        gapi.client.load('bigquery', 'v2');
+        $('#container').show();
+        return $('#auth').hide();
+      });
+    });
+    return $('#run').on('click', function() {
+      var request;
+      $('#information').text('loading...');
+      if ($('#result tbody').size() > 0) {
+        $('#result').dataTable().api().destroy();
+        $('#result').empty();
+      }
+      request = gapi.client.bigquery.jobs.query({
+        projectId: project_id,
+        query: $('#query').val()
+      });
+      return request.execute(function(response) {
+        var columns, data;
+        if (response.error) {
+          return $('#information').text(response.message);
+        } else {
+          $('#information').empty();
+          columns = response.schema.fields.map(function(field) {
+            return {
+              title: field.name
+            };
+          });
+          data = _.map(response.rows, function(row) {
+            return _.map(row.f, function(values) {
+              return values.v;
+            });
+          });
+          return $('#result').dataTable({
+            data: data,
+            columns: columns
+          });
+        }
       });
     });
   });
